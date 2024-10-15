8571e077799e5cea008ad2025b2c7d419eb35bed
Nobinwanne <ndidiobinwanne@gmail.com>
2024-10-15 13:51:08 -0600

    added Heroku

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express();
 
 const PORT = process.env.PORT || 5050;
 console.log(PORT);
+console.log("process.env:", process.env);
 
 app.get("/", (req, res) => {
-  res.send(`<h1>Welcome to my Express App</h1>`);
+  res.send(`<h1>Welcome to my Express App on Heroku </h1>`);
 });
 
 app.listen(PORT, () => {
